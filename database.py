9ac093ea8cd2c63d5dfd26fa4b084bc935a5032e
adityach16 <130712757+adityach16@users.noreply.github.com>
2023-05-06 05:02:29 +0530

    Update database.py
    production database added

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgres://yqbjvepw:7_OJ8gIWle9fEyoWiNr-W31ZVIqm3G-f@tiny.db.elephantsql.com/yqbjvepw"
+SQLALCHEMY_DATABASE_URL = "postgresal://yqbjvepw:7_OJ8gIWle9fEyoWiNr-W31ZVIqm3G-f@tiny.db.elephantsql.com/yqbjvepw"
 
 
 
