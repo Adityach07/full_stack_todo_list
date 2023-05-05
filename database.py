3695100a6ef9ba33d06714f110cdd13e8b62aa84
adityach16 <130712757+adityach16@users.noreply.github.com>
2023-05-05 06:28:43 +0530

    Update database.py
    error rectified

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = ""postgresql://kukeqqcd:s3anFmwOyaKvIvVdfEo2wf6MQyBTJmTL@tiny.db.elephantsql.com/kukeqqcd"
+SQLALCHEMY_DATABASE_URL = "postgresql://kukeqqcd:s3anFmwOyaKvIvVdfEo2wf6MQyBTJmTL@tiny.db.elephantsql.com/kukeqqcd"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
